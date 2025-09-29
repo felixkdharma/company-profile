707d7580a20a15fc8b5c8faed8ec0f6027856913
Felix Kurniawan Dharma <felix.dharma121299@gmail.com>
2025-09-29 08:18:07 +0700

    Section homepage sudah ada gambar dan judul, lagi dibuat responsiv

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,36 @@
+import "../styles/global.css"
+import "../styles/home.css"
+import { Flex } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
+
 function App() {
 
   return (
     <section id="homepage">
-      <img
-        src="../../../src/assets/homepage.jpg"
-        alt="homepage"
-        style={{
-          display: "block",
-          width: "100%",
-          height: "auto",
-          objectFit: "cover",
-          objectPosition: "center",
-        }} />
-      <h1> Hello World </h1>
+
+      <div className="title">
+        <Flex gap={{ "2xl": 12, xl: 12, }}
+          alignItems={"center"}
+          justifyContent={"center"} >
+          <Box
+            w="500px"
+            marginLeft={12}
+            paddingTop={"20%"}
+            paddingLeft={{
+              "2xl": "15%",
+              "xl" : "15%",
+              "lg" : "15%"
+            }}
+            textWrap={{"lg" : "wrap"}}>
+            <h1> PT Adi Dharma </h1>
+          </Box>
+
+          <Box w="500px" paddingTop={"20%"}>
+            <h1> Flexibel Creative Innovative </h1>
+          </Box>
+
+        </Flex>
+      </div>
 
     </section>
   )
